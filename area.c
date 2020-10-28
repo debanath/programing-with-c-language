b73d334b662b284ca21bd5b2f9c89c56be511e0a
Debashish Nath <debanath26@gmail.com>
2020-10-29 01:14:13 +0530

    area

---
+++ area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+
+    float l, b, area; // ! l= length, b= breadth, area = area
+
+        printf("Enter Length and Breadth: ");
+        scanf("%f %f", &l, &b);
+
+            area= l*b;
+
+        printf("Area of the rectangle = %f ", area);
+
+}
